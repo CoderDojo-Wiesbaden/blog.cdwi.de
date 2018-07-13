9b5de80da8d56d1f255111024b47f659c464beaa
koenidv <32238636+koenidv@users.noreply.github.com>
2018-07-13 13:27:19 +0200

    Mobile: Smooth scrolling, design changes

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+document.getElementById("pulltab").classList.remove("invisible");
+
+$(document).ready(function () {
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 400, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
+});
+
+function fixHoverState() {
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function() {par.insertBefore(el, next);}, 0)
+}
